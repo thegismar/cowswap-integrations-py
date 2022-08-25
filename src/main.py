5436b7671fe6418857eb4758009efe8fcc038c10
thegismar <me@billions.sh>
2022-08-25 18:29:36 -0300

    fixed some things, added brownie to requirements.txt

--- src/main.py
+++ src/main.py
@@ -1,20 +1,36 @@
 from src.fee import Fee
 from src.order import Order
 from src.domains import private_key
+from brownie import *
 
 
 def cow_swap():
-    # fill variables below and fill network, public address and private key in .env
-    sell_token = "0xDDAfbb505ad214D7b80b1f830fcCc89B60fb7A83"
-    buy_token = "0x4ecaba5870353805a9f068101a40e0f32ed605c6"
-    sell_amount = 1000000
-    buy_amount = 1000000
-    kind = "buy"
+    # brownie stuff
+    network.connect('mainnet')
+    account = accounts.add(private_key=private_key)
+    sell_token = "0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32"  # LDO
+    buy_token = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"  # WETH
 
+    sell_token_contract = Contract.from_explorer(sell_token)
+    sell_amount = sell_token_contract.balanceOf(account)
+
+    ## TODO GET QUOTE FOR BUY_AMOUNT
+    buy_amount = 1  # 0 seems to cause error, 1 wei is the minimum amount, let's assume there's no shennanigans so it
+    # should give us the highest amount anyways
+
+    kind = "sell"
+
+    ## fee calc seems to work right
     fee = Fee(sell_token, buy_token, sell_amount, kind, buy_amount)
     fee_amount = fee.get_fee()
-    order = Order(sell_token, buy_token, sell_amount, buy_amount, fee_amount, kind)
-    order.sign(private_key)
+
+    # sell amount less the fees, which imo are always taken out of the sell amount, never the buy
+    order = Order(sell_token, buy_token, sell_amount - fee_amount, buy_amount, fee_amount, kind)
+
+    # no need to put the private key here since it's already in the environment variables (a throwaway wallet one ofc)
+    order.sign()
+
+    # should return a hex that corresponds to the order id on cow protocol explorer
     print(order.post_order())
 
 
